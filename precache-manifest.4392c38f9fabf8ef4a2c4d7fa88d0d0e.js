be8d66705d9a977171607f22075732837f29a79a
Denis Butsky <nthbutsky@gmail.com>
2022-01-20 10:21:16 +0200

    deploy

---
+++ precache-manifest.4392c38f9fabf8ef4a2c4d7fa88d0d0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c75f4c2d8f1e09eed7a",
+    "url": "/inchkiev-test/css/app.6628a0cc.css"
+  },
+  {
+    "revision": "8afe4755ab6ff9f2eeac37cdee134494",
+    "url": "/inchkiev-test/img/dog.8afe4755.jpg"
+  },
+  {
+    "revision": "7078ca263a7735bd18bbcde5710481b6",
+    "url": "/inchkiev-test/img/star.7078ca26.svg"
+  },
+  {
+    "revision": "ec5df875ce9dc47b063fa10546b510e7",
+    "url": "/inchkiev-test/index.html"
+  },
+  {
+    "revision": "4c75f4c2d8f1e09eed7a",
+    "url": "/inchkiev-test/js/app.eef63de2.js"
+  },
+  {
+    "revision": "4b6e337c30658fbea679",
+    "url": "/inchkiev-test/js/chunk-vendors.3ce95c64.js"
+  },
+  {
+    "revision": "548c5790e3dd75e739780a1668a6c89d",
+    "url": "/inchkiev-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/inchkiev-test/robots.txt"
+  }
+]);
